b49a82d098960138ff53023135c7e7a25f86b40c
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-30 23:14:55 +0530

    creating inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Build A Hero</h1>
+      <label>Name: </label>
+      <input type="text" />
+      <label>Age: </label>
+      <input type="number" />
+      <label>Height: </label>
+      <input type="text" />
+      <label>SuperPower: </label>
+      <input type="text" />
     </div>
   );
 }
