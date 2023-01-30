f4a22c0a7c795c969d45d23c8ea113d0d51dfa99
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-30 23:32:06 +0530

    changing states

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('David');
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [superPower, setSuperPower] = useState('');
 
-  const changeName = () => {
-    setName('John');
-  };
   return (
     <div className="App">
       <h1>Build A Hero</h1>
       <label>Name: </label>
-      <input type="text" style={{ margin: 10 }} />
+      <input
+        type="text"
+        style={{ margin: 10 }}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>Age: </label>
-      <input type="number" style={{ margin: 10 }} />
+      <input
+        type="number"
+        style={{ margin: 10 }}
+        onChange={(e) => setAge(e.target.value)}
+      />
       <label>Height: </label>
-      <input type="text" style={{ margin: 10 }} />
+      <input
+        type="number"
+        style={{ margin: 10 }}
+        onChange={(e) => setHeight(e.target.value)}
+      />
       <label>SuperPower: </label>
-      <input type="text" style={{ margin: 10 }} />
-
-      <button onClick={changeName}>Change Name</button>
+      <input
+        type="text"
+        style={{ margin: 10 }}
+        onChange={(e) => setSuperPower(e.target.value)}
+      />
       {name}
+      {age}
+      {height}
+      {superPower}
     </div>
   );
 }
